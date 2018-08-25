8af1fd573f5f10267dc065e48e0c7397ca9362d7
Adèle Mortier <adele.mortier@gmail.com>
2018-08-25 02:49:51 +0200

    did some corrections after review

--- compil.sh
+++
@@ -1,9 +0,0 @@
-rm *.aux
-rm *.bbl
-rm *.blg
-pdflatex adele_mortier_MEMOIRE_M1.tex
-bibtex adele_mortier_MEMOIRE_M1.aux
-pdflatex  adele_mortier_MEMOIRE_M1.tex
-bibtex adele_mortier_MEMOIRE_M1.fb.aux
-pdflatex adele_mortier_MEMOIRE_M1.tex
-xdg-open adele_mortier_MEMOIRE_M1.pdf > /dev/null
